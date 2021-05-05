0adb7d3f6f3aea8181b7de672b0e3903f35c15b2
Erick Nolasco <erocnolascoma@ittepic.edu.mx>
2021-05-05 17:49:09 -0600

    Funciones de buscar, editar y borrar

--- index.js
+++ index.js
@@ -29,3 +29,82 @@ perfil.save(function (error) {
     console.log('Se ha creado el perfil con éxito');
     process.exit(0);
 });
+
+Perfil.save(function (error) {
+    if (error) {
+        console.log(error);
+        process.exit(1);
+    }
+    console.log('Guardado con éxito');
+
+    //-------------------------------------------------------
+    Perfil.find(function (error, docs) {
+        if (error) {
+            console.log(error);
+            process.exit(1);
+        }
+        console.log('Consulta General');
+        console.log(docs);
+
+        //-------------------------------------------------------
+        Perfil.find({ _id: '' },
+            function (error, docs) {
+                if (error) {
+                    console.log(error);
+                    process.exit(1);
+                }
+                console.log('Consulta por ID');
+                console.log(docs);
+
+                //-------------------------------------------------------
+                Perfil.update({ _id: '' },
+                    { $set: { hidden: false } },
+                    function (error, docs) {
+                        if (error) {
+                            console.log(error);
+                            // process.exit(1);
+
+                        }
+                        console.log('Actualizacion por ID');
+                        console.log(docs);
+
+                        //-------------------------------------------------------
+                        Perfil.find(function (error, docs) {
+                            if (error) {
+                                console.log(error);
+                                process.exit(1);
+                            }
+                            console.log('Consulta general despues de actualizar');
+                            console.log(docs);
+
+                            //-------------------------------------------------------
+                            Perfil.findByIdAndRemove({ _id: '' },
+
+                                function (error, docs) {
+                                    if (error) {
+                                        console.log(error);
+                                        // process.exit(1);
+
+                                    }
+                                    console.log('Eliminar por ID');
+                                    console.log(docs);
+
+                                    //-------------------------------------------------------
+                                    Perfil.find(function (error, docs) {
+                                        if (error) {
+                                            console.log(error);
+                                            process.exit(1);
+                                        }
+                                        console.log('Consulta general despues de eliminar');
+                                        console.log(docs);
+                                    });
+                                });
+                        });
+
+                    });
+
+            });
+
+    });
+
+});
